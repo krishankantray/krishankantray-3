1a9acf09c06aa2facdd3df9ffcee660e0d5e49ed
Krishankant Ray <krishankant_rai@rediffmail.com>
2020-08-23 14:55:22 +0530

    404 page update

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import NotFound from '../images/404.svg' 
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <img src={NotFound} height={400} width={500} />
   </Layout>
 )
 
