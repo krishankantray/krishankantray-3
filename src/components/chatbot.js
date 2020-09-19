a01ba260774328005472699120b4fc1774f40421
Krishankant Ray <krishankant_rai@rediffmail.com>
2020-09-19 18:10:50 +0530

    Update chatbot.js

--- src/components/chatbot.js
+++ src/components/chatbot.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ChatBot from "react-simple-chatbot";
 import avatar from "../images/botImages/avatar.png";
+import { Link } from "gatsby";
+
 function CustomChatbot(props) {
   const config = {
     width: "300px",
@@ -12,22 +14,41 @@ function CustomChatbot(props) {
   const steps = [
     {
       id: "Greet",
-      message: "Hello, Welcome to our shop",
-      trigger: "Ask Name",
+      message: "Hello, Welcome to my portfolio site",
+      trigger: "Welcome page message",
     },
     {
-      id: "Ask Name",
-      message: "Please type your name?",
-      trigger: "Waiting user input for name",
+      id: "Welcome page message",
+      message: "How can I help you navigate though me site",
+      trigger: "Options for welcome page",
     },
     {
-      id: "Waiting user input for name",
-      user: true,
-      trigger: "Asking options to eat",
+      id: "Options for welcome page",
+      options: [
+          { value: 1, label: 'Resume', trigger: '4' },
+          { value: 2, label: 'Projects', trigger: '5' },
+          { value: 3, label: 'Blogs', trigger: '6' },
+        ],
     },
     {
-      id: "Asking options to eat",
-      message: "Hi {previousValue}, Glad to know you !!",
+      id: '4',
+      component: (
+        <Link to="/resume">Visit Resume </Link>
+      ),
+      end: true,
+    },
+    {
+      id: '5',
+      component: (
+        <Link to="/showcase">See Projects </Link>
+      ),
+      end: true,
+    },
+    {
+      id: '6',
+      component: (
+        <Link to="/contact">See Contact details </Link>
+      ),
       trigger: "Done",
     },
     {
